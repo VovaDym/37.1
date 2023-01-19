c98d9e28eee3c0ab05a16b841d53961fe29a5c25
Vladimir Dymov <lebed19732212@gmail.com>
2023-01-19 19:23:47 +0300

    37.1.Ok.OtimizationSlots1

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@ int main(int argc, char *argv[]) {
     Ui::MainWindow calculator;
     calculator.setupUi(&window);
     window.resultShow = calculator.resultShow;
-//    window.button = calculator.pushButton_0;
-//    window.button = calculator.pushButton_1;
-//    window.button = calculator.pushButton_3;
     window.resize(480, 640);
     window.show();
     return QApplication::exec();
